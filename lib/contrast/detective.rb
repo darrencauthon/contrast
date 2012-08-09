c94206f8105b4c6e1288fe21c89827d0150d9ea4
Darren Cauthon <darren@cauthon.com>
2012-08-08 19:47:38 -0500

    Get basic comparisons to work.

--- lib/contrast/detective.rb
+++ lib/contrast/detective.rb
@@ -1,10 +1,15 @@
 module Contrast
   class Detective
     def initialize(*args)
+      @fields = args
     end
 
     def examine(a, b)
-      []
+      results = []
+      @fields.each do |field|
+        results << field if a.send(field) != b.send(field)
+      end
+      results
     end
   end
 end
--- spec/contrast/detective_spec.rb
+++ spec/contrast/detective_spec.rb
@@ -6,11 +6,113 @@ describe Contrast::Detective do
       before do
         @a = [:name].to_object
         @b = [:name].to_object
-        @detective = Contrast::Detective.new :name
+        @a.name = ""
+        @b.name = ""
+        detective = Contrast::Detective.new :name
+        @result = detective.examine(@a, @b)
       end
 
       it "should return no results" do
-        @detective.examine(@a, @b).count.must_equal 0
+        @result.count.must_equal 0
+      end
+    end
+
+    describe "two objects that do not match" do
+      before do
+        @a = [:name].to_object
+        @b = [:name].to_object
+        @a.name = 'apple'
+        @b.name = 'orange'
+        detective = Contrast::Detective.new :name
+        @result = detective.examine(@a, @b)
+      end
+
+      it "should return one result" do
+        @result.count.must_equal 1
+      end
+
+      it "should return the field" do
+        @result[0].must_equal :name
+      end
+    end
+  end
+
+  describe "two methods comparison" do
+    describe "two objects that match" do
+      before do
+        @a = [:first_name, :last_name].to_object
+        @b = [:first_name, :last_name].to_object
+        objects = [:first_name, :last_name].to_objects {[
+          ['a', 'b'], ['a', 'b']
+        ]}
+
+        detective = Contrast::Detective.new :first_name, :last_name
+        @result = detective.examine(objects[0], objects[1])
+      end
+
+      it "should return no results" do
+        @result.count.must_equal 0
+      end
+    end
+
+    describe "two objects that do not match by first property" do
+      before do
+        @a = [:first_name, :last_name].to_object
+        @b = [:first_name, :last_name].to_object
+        objects = [:first_name, :last_name].to_objects {[
+          ['a', 'b'], ['z', 'b']
+        ]}
+        detective = Contrast::Detective.new :first_name, :last_name
+        @result = detective.examine(objects[0], objects[1])
+      end
+
+      it "should return one result" do
+        @result.count.must_equal 1
+      end
+
+      it "should return the field" do
+        @result[0].must_equal :first_name
+      end
+    end
+
+    describe "two objects that do not match by second property" do
+      before do
+        @a = [:first_name, :last_name].to_object
+        @b = [:first_name, :last_name].to_object
+        objects = [:first_name, :last_name].to_objects {[
+          ['a', 'b'], ['a', 'c']
+        ]}
+        detective = Contrast::Detective.new :first_name, :last_name
+        @result = detective.examine(objects[0], objects[1])
+      end
+
+      it "should return one result" do
+        @result.count.must_equal 1
+      end
+
+      it "should return the field" do
+        @result[0].must_equal :last_name
+      end
+    end
+
+    describe "two objects that do not match by both properties" do
+      before do
+        @a = [:first_name, :last_name].to_object
+        @b = [:first_name, :last_name].to_object
+        objects = [:first_name, :last_name].to_objects {[
+          ['a', 'b'], ['y', 'z']
+        ]}
+        detective = Contrast::Detective.new :first_name, :last_name
+        @result = detective.examine(objects[0], objects[1])
+      end
+
+      it "should return one result" do
+        @result.count.must_equal 2
+      end
+
+      it "should return the fields" do
+        @result[0].must_equal :first_name
+        @result[1].must_equal :last_name
       end
     end
   end
