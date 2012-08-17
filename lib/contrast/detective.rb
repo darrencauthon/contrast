143eca068a28948a6517d8b8d7b8eb826a083fa1
Darren Cauthon <darren@cauthon.com>
2012-08-16 21:56:01 -0500

    Cleanup detective.

--- lib/contrast/detective.rb
+++ lib/contrast/detective.rb
@@ -7,20 +7,21 @@ module Contrast
     end
 
     def examine(a, b)
-      keys = @fields.select do |field| 
+      Hash[fields_that_do_not_match(a, b).map do |key|
+        [key, { :actual_value => get_the_value(a, key),
+                :expected_value => get_the_value(b, key) }]
+      end]
+    end
+
+    private
+
+    def fields_that_do_not_match(a, b)
+      @fields.select do |field| 
         first = get_the_value(a, field)
         second = get_the_value(b, field)
         the_values_the_do_not_match(first, second)
       end
-      result = {}
-      keys.each do |key|
-        result[key] = {:actual_value => get_the_value(a, key),
-                       :expected_value => get_the_value(b, key)}
-      end
-      result
     end
-
-    private
 
     def get_the_value(object, field)
       begin
